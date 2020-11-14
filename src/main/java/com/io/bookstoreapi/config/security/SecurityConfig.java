891a0e3c4f211b50094aedeb4ffa3c44873448d1
Blazej Wylegly <blazej.wylegly@gmail.com>
2020-11-14 15:18:53 +0100

    Added global cors configuration

--- src/main/java/com/io/bookstoreapi/config/security/SecurityConfig.java
+++ src/main/java/com/io/bookstoreapi/config/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.io.bookstoreapi.config.security;
 
 import com.io.bookstoreapi.data.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +25,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Autowired
+    @Qualifier("corsConfigurer")
+    private WebMvcConfigurer webMvcConfigurer;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
